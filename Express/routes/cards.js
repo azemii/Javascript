5a11b3466decdd254a96e79cbed8b45fedfb0780
Armend Azemi <aa223ee@student.lnu.se>
2022-08-01 00:08:37 +0200

    Added stud data set

--- Express/routes/cards.js
+++ Express/routes/cards.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// * The brackets are the equivilent of cards = data.cards.
+//* The text file is by default parsed into JSON when using node, we don't need to parse it ourselves.
+const { data } = require('../data/flashCardData.json');
+const { cards } = data;
+
 /* 
 * Since all '/cards' routes are being directed into this file, we don't need
 * to specify router.get('/cards'), we can simply just use '/' instead.
 */
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     //* Another way to access dyamic values on our template.
     // res.locals.prompt = 'Who is buried in a random tomb?'
-    res.render('card', {prompt: 'Who has won the most championships in F1 racing?', hint: 'He is from the UK'});
+    res.render('card', {
+        prompt: cards[req.params.id].question, 
+        hint: cards[req.params.id].hint
+    });
 });
 
 module.exports = router;
