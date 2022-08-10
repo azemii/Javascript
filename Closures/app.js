30e61bee38e485d57111532fa04b1460d098db4d
Armend Azemi <aa223ee@student.lnu.se>
2022-08-10 23:50:13 +0200

    Small closure example

--- Closures/app.js
+++ Closures/app.js
@@ -2,6 +2,7 @@
 //* When the inner function is stored in a variable let k = makeCounter()
 //* then all the variables inside both functions are stored in memeory
 //* Each seperate function call that is stored in a new varaible has it's own local scope.
+let buttons = document.getElementsByTagName('button');
 
 function makeCounter(name){
     var count = 0;
@@ -11,3 +12,16 @@ function makeCounter(name){
     }
 }
 
+
+function createHandler(name){
+    return function(){
+        console.log(name);
+    }
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    let buttonName = button.innerHTML;
+    button.addEventListener('click', createHandler(buttonName));
+    
+}
