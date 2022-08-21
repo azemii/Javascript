a059eabb4e4f0acd43011037cb7ea2c4565c6795
Armend Azemi <aa223ee@student.lnu.se>
2022-08-21 13:45:35 +0200

    Removed unnessecary code.

--- Express/app.js
+++ Express/app.js
@@ -29,9 +29,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    next();
-});
 /*
 * Throws a 404 error when no route has matched a reqeust.
 * Should be placed at the end, after all routes have been defined. 
